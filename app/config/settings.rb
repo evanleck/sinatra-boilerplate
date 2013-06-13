156064c34ab68437b07650f5db09bd14263d0e22
Evan Lecklider <evan.lecklider@gmail.com>
2013-06-13 11:54:28 -0700

    Added email notification addresses to the settings file instead of inline.

--- app/config/settings.rb
+++ app/config/settings.rb
@@ -1,10 +1,12 @@
 configure do
   # = Configuration =
-  set :run,             false
-  set :server,          %w(unicorn)
-  set :show_exceptions, false
-  set :raise_errors,    development?
-  set :views,           File.join(settings.app_root, 'app', 'views')
-  set :logging,         true
-  set :static,          false # your upstream server should deal with those (nginx, Apache)
+  set :run,               false
+  set :server,            %w(unicorn)
+  set :show_exceptions,   false
+  set :raise_errors,      development?
+  set :views,             File.join(settings.app_root, 'app', 'views')
+  set :logging,           true
+  set :static,            false # your upstream server should deal with those (nginx, Apache)
+  set :notification_to,   %w(you@example.com)
+  set :notification_from, 'service@example.com'
 end
--- app/helpers/notification_helpers.rb
+++ app/helpers/notification_helpers.rb
@@ -2,19 +2,19 @@ module Sinatra
   module NotificationHelpers
     def notify_devs(err)
       exception = err
-      from      = 'service@example.com'
+      from      = settings.notification_from
       subject   = "A #{ exception.class.to_s } Occurred on Your Rack Application"
       body      = ERB.new(EMAIL_TEMPLATE).result(binding) # not sure about this (binding) thing
       html_body = ERB.new(HTML_EMAIL_TEMPLATE).result(binding)
-    
+
       # loop and email
-      ['you@example.com'].each do |email|
+      settings.notification_to.each do |email|
         Pony.mail :to => email, :from => from, :subject => subject, :body => body, :html_body => html_body, :via => :sendmail
       end
     rescue
       # shhhh
     end
   end
-  
+
   helpers NotificationHelpers
 end
